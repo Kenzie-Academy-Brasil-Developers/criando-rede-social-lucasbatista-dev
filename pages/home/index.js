22fb372eb676c597077ea11012d00950565a94b9
lucasbatista-dev <lucas_pivatto@hotmail.com.br>
2022-09-23 19:09:07 -0300

    firstmobile concluido

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,2 +1,10 @@
-/* Desenvolva a lógica da página aqui */
+const buttonsControllersModal = document.querySelectorAll(
+  "[data-control-modal]"
+);
+for (let i = 0; i < buttonsControllersModal.length; i++) {
+  buttonsControllersModal[i].addEventListener("click", () => {
+    let modalId = buttonsControllersModal[i].getAttribute("data-control-modal");
 
+    document.getElementById(modalId).classList.toggle("show__modal");
+  });
+}
